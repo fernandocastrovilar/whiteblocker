c648877e09f22f1ce137102184f683bb55aba705
fcastro <fcastro@idealista.com>
2019-08-13 08:29:58 +0200

    Functional notifications

--- common/NotifyUtils.py
+++ common/NotifyUtils.py
@@ -1,19 +1,26 @@
 import smtplib
 
-smtp_server = "smtp.gmail.com"
-smtp_user = "" # Gmail user account
-smtp_pass = "" # Gmail password account
 
-RECIPIENTS = "" # Email to:
-SENDER = smtp_user
-mssg = ""
-s = mssg
+# Function for send email using an existing gmail account
+def send_email(recipient, msg):
+	smtp_server = "smtp.gmail.com"
 
-server = smtplib.SMTP(smtp_server, 587)
-server.ehlo()
-server.starttls()
-server.ehlo()
-server.login(smtp_user, smtp_pass)
-server.set_debuglevel(1)
-server.sendmail(SENDER, [RECIPIENTS], s)
-server.quit()
+	with open("credentials.txt") as f:
+		credentials = [x.strip().split(':', 1) for x in f]
+	for username, password in credentials:
+		smtp_user = username
+		smtp_pass = password
+
+	recipients = recipient
+	sender = smtp_user
+	subject = "New event WhiteBlocker"
+	text = msg
+	message = "Subject: {0}\n\n{1}".format(subject, text)
+
+	server = smtplib.SMTP(smtp_server, 587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+	server.login(smtp_user, smtp_pass)
+	server.sendmail(sender, recipients, message)
+	server.quit()
